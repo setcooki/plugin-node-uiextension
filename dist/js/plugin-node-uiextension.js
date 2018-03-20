3345f00001ab3a729b89a3b8793e29c62d530cb7
setcookie <set@cooki.me>
2018-03-20 12:49:07 +0100

    fix class selector

--- dist/js/plugin-node-uiextension.js
+++ dist/js/plugin-node-uiextension.js
@@ -5,11 +5,11 @@ var PluginUIExtension = {
    * The function defined as the onready callback within the plugin configuration.
    */
   init: function () {
-    var $nav = $('#pl-pattern-nav-target');
+    var $nav = $('.pl-c-nav__list');
     $nav.prepend(/*NAVLINKS-BEFORE-SNIPPET*/);
     $nav.append(/*NAVLINKS-AFTER-SNIPPET*/);
 
-    var $rightList = $('.sg-checklist');
+    var $rightList = $('.pl-c-tools__list');
     $rightList.prepend(/*GEARLINKS-BEFORE-SNIPPET*/);
     $rightList.find('#sg-find').before(/*GEARLINKS-BEFORESEARCH-SNIPPET*/);
   }
